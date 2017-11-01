9ecf94f0b0bca46d2c14f3e1ee5a39c16758099a
tiancl <chenglong.tian@baifendian.com>
2017-11-01 15:50:51 +0800

    添加了resource资源类

---
+++ src/main/java/com/eliteams/quick4j/web/security/ResourceSign.java
@@ -0,0 +1,119 @@
+package com.eliteams.quick4j.web.security;
+
+/**
+ * 系统资源,模块
+ * 
+ * @author StarZou
+ * @since 2014年6月17日 下午4:03:33
+ **/
+public class ResourceSign {
+    /**
+     * 人员
+     */
+    public static final String PERSONNEL = "personnel";
+    
+    
+    /**
+     * 机构
+     */
+    public static final String  ORGANIZATION= "organization";
+    
+    
+    /**
+     * 收入
+     */
+    public static final String INCOME="income";
+    
+    
+    /**
+     * 利润
+     */
+    public static final String PROFIT = "profit";
+    
+    
+    /**
+     * 宏观战略
+     */
+    public static final String MACRO="macro";
+    
+    /**
+     * 基础管理
+     */
+    
+    public static final String BASIC = "basic";
+    
+    /**
+     * 财务管理
+     */
+    
+    public static final String FINANCIAL="financial";
+    
+    /**
+     * 业务管理
+     */
+    
+    public static final String 	BUSINESS="business";
+    
+    
+    /**
+     * 管理员
+     */
+    
+    public static final String ADMINSTRATOR="administrator";
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
